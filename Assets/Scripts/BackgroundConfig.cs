cb9f78aee2b2c801b3f81eeb9ae38ab72e0dfa9a
UnityLV <104660372+UnityLV@users.noreply.github.com>
2023-04-04 18:03:46 +0300

    Add Background
    Add in Match 3 scene baground
    from curent level

---
+++ Assets/Scripts/BackgroundConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class BackgroundConfig
+{
+    [field: SerializeField] public Sprite Sprite { get; private set; }
+    [field: SerializeField] public int StartLevel { get; private set; }
+    [field: SerializeField] public int EndLevel { get; private set; }
+
+}
---
+++ Assets/Scripts/BackgroundSetter.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundSetter : MonoBehaviour
+{
+    [SerializeField] private Image _bagroundImage;
+    [SerializeField] private BackgroundConfig[] _configs;
+
+    private void Start()
+    {
+        SetBaground();
+    }
+
+    private void SetBaground()
+    {
+        _bagroundImage.sprite = GetBackground();
+    }
+
+    private Sprite GetBackground()
+    {
+        foreach (var config in _configs)
+        {
+            if (IsSutableConfig(config))
+            {
+                return config.Sprite;
+            }
+        }
+
+        return GetHighestLevelBackground();
+    }
+
+    private bool IsSutableConfig(BackgroundConfig config)
+    {
+        return config.StartLevel <= LevelSelector.SelectedLevel &&
+            config.EndLevel >= LevelSelector.SelectedLevel;
+    }
+
+    private Sprite GetHighestLevelBackground()
+    {
+        return _configs.OrderByDescending(c => c.EndLevel).Select(c => c.Sprite).First(); ;
+    }
+}
